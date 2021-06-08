0771a4246871bf70e477233bc4f9686cb31df6ab
Jacopo Gasparetto <jacopo.gasparetto@gmail.com>
2021-06-08 18:11:14 +0200

    Refactor: removed unused Runnable.h/.cpp Minor code cleaning refactor

--- src/Backend.cpp
+++ src/Backend.cpp
@@ -5,22 +5,14 @@
 #include "Backend.h"
 
 Backend::Backend(QObject *parent)
-    : QObject{parent}{
-    m_runnable = new Runnable(this);
-    m_runnable->setAutoDelete(false);
+        : QObject{ parent } {
     std::cout << "Instantiated backend helper\n";
 }
 
 Backend::~Backend() {
-    m_runnable->stop();
-    delete m_runnable;
-    std::cout << "runnable deleted, removing backend\n";
     disconnectFromHost();
 }
 
-double Backend::getValue() const {
-    return m_value;
-}
 
 bool Backend::connectToHost(const QString &host, uint16_t port) {
     startListening();
@@ -39,16 +31,15 @@ void Backend::disconnectFromHost() {
     if (m_isPinging)
         togglePingUpdate();
 
-    bool prevIsConnected {isConnected()};
+    bool prevIsConnected{ isConnected() };
     ClientInterface<MsgTypes>::disconnect();
+
     if (prevIsConnected != isConnected())
-        emit connectionStatusChanged(isConnected());
+            emit connectionStatusChanged(isConnected());
 
     if (!isConnected()) {
-        std::cout << "[BACKEND]: disconnected from host" << std::endl;
+        std::cout << "[BACKEND] Disconnected from host" << std::endl;
     }
-
-
 }
 
 void Backend::sendGreetings() {
@@ -66,36 +57,40 @@ void Backend::onConnectionFailed(std::error_code &ec) {
 void Backend::onMessage(message<MsgTypes> &msg) {
     switch (msg.header.id) {
         case MsgTypes::ServerAccept: {
-            std::cout << "Server accepted\n";
+            std::cout << "[BACKEND] Server accepted\n";
             emit connectionStatusChanged(true);
             break;
         }
 
         case MsgTypes::ServerMessage: {
-            std::cout << "Message from server: " << msg << ":\n";
+            std::cout << "[BACKEND] Message from server: " << msg << ": ";
             std::ostringstream out;
-            for (auto ch: msg.body) {
+
+            for (auto ch: msg.body)
                 out << ch;
-            }
+
             std::cout << out.str() << std::endl;
-        }
             break;
+        }
 
         case MsgTypes::ServerPing: {
             std::chrono::system_clock::time_point timeNow = std::chrono::system_clock::now();
             std::chrono::system_clock::time_point timeThen;
-            msg >> timeThen;
-            m_lastPingValue = std::chrono::duration<double>(timeNow - timeThen).count() * 1000;
 
+            msg >> timeThen;
+
+            m_lastPingValue = std::chrono::duration<double>(timeNow - timeThen).count() * 1000;
             std::cout << "Ping: " << m_lastPingValue << " ms.\n";
 
             emit pingReceived(m_lastPingValue);
-        }
-            break;
 
-        case MsgTypes::ClientPing:
+            break;
+        }
+
+        case MsgTypes::ClientPing: {
             send(msg);
             break;
+        }
 
         default:
             std::cout << "???\n";
@@ -105,6 +100,7 @@ void Backend::onMessage(message<MsgTypes> &msg) {
 // Helpers
 void Backend::pingHandler() {
     std::cout << "[BACKEND] Ping thread started\n";
+
     while (m_isPinging) {
         message<MsgTypes> pingMsg;
         pingMsg.header.id = ServerPing;
@@ -122,20 +118,10 @@ void Backend::togglePingUpdate() {
         m_pingThread.detach();
     } else {
         m_isPinging = true;
-        m_pingThread = std::thread{&Backend::pingHandler, this};
+        m_pingThread = std::thread{ &Backend::pingHandler, this };
     }
 }
 
 double Backend::getLastPingValue() const {
     return m_lastPingValue;
-}
-
-// Slots
-void Backend::setNumber(int t) {
-    if (t == m_t)
-        return;
-
-    m_t = t;
-    m_value = (1.0 + std::sin(static_cast<double>(m_t) / 50.0)) / 2.0;
-    emit valueChanged(m_value);
 }
